5da425b8b33f9a8cdccd520ab5d180e2643be003
elliotec <elliotecweb@gmail.com>
2014-03-02 21:02:29 -0800

    chap 7

--- lib/rulers/sqlite_model.rb
+++ lib/rulers/sqlite_model.rb
@@ -37,14 +37,17 @@ SQL
         data["id"] = DB.execute(sql)[0][0]
         self.new data
       end
+
       def self.count
     DB.execute(<<SQL)[0][0]
 SELECT COUNT(*) FROM #{table}
 SQL
       end
+
       def self.table
         Rulers.to_underscore name
       end
+
       def self.schema
         return @schema if @schema
         @schema = {}
@@ -52,6 +55,46 @@ SQL
           @schema[row["name"]] = row["type"]
         end
         @schema
+      end
+
+      def self.find(id)
+        row = DB.execute <<SQL
+select #{schema.keys.join ","} from #{table}
+where id = #{id};
+SQL
+        data = Hash[schema.keys.zip row[0]]
+        self.new data
+      end
+
+      def [](name)
+        @hash[name.to_s]
+      end
+
+      def []=(name, value)
+        @hash[name.to_s]=value
+      end
+
+
+      def save!
+        unless @hash["id"]
+          self.class.create
+          return true
+        end
+
+        fields = @hash.map do |k,v|
+          "#{k}=#{self.class.to_sql(v)}"
+        end.join ","
+
+        DB.execute <<SQL
+UPDATE #{self.class.table}
+SET #{fields}
+WHERE id = #{@hash["id"]}
+SQL
+        true
+      end
+
+      def save
+        self.save! rescue false
       end
     end
   end
